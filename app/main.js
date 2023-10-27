a7b1e87a3a898e9e0452ec9c850503bff8eb5088
Sebin CM <sebinsebzz2002@gmail.com>
2023-10-27 16:03:39 +0530

    Post a file

--- app/main.js
+++ app/main.js
@@ -6,9 +6,7 @@ const server = net.createServer(handleClient);
 function handleClient(socket) {
   socket.on("data", (data) => {
     const request = data.toString();
-    const { path, userAgent } = parseRequest(request);
-
-    console.log(`Request: ${path} (${userAgent})`);
+    const { requestType, path, userAgent, body } = parseRequest(request);
 
     if (path === "/") {
       sendResponse(socket, 200, "OK", "text/plain", "");
@@ -24,13 +22,36 @@ function handleClient(socket) {
       }
       const dir = process.argv[dirIndex];
       const fileName = path.substring(7);
-      fs.readFile(`${dir}/${fileName}`, (err, data) => {
-        if (err) {
-          sendResponse(socket, 404, "Not Found", "text/plain", "404 Not Found");
-          return;
-        }
-        sendResponse(socket, 200, "OK", "application/octet-stream", data);
-      });
+      if (requestType === "GET") {
+        fs.readFile(`${dir}/${fileName}`, (err, data) => {
+          if (err) {
+            sendResponse(
+              socket,
+              404,
+              "Not Found",
+              "text/plain",
+              "404 Not Found"
+            );
+            return;
+          }
+          sendResponse(socket, 200, "OK", "application/octet-stream", data);
+        });
+      }
+      if (requestType === "POST") {
+        fs.writeFile(`${dir}/${fileName}`, body, (err) => {
+          if (err) {
+            sendResponse(
+              socket,
+              404,
+              "Not Found",
+              "text/plain",
+              "404 Not Found"
+            );
+            return;
+          }
+          sendResponse(socket, 201, "OK", "text/plain", body);
+        });
+      }
     } else {
       sendResponse(socket, 404, "Not Found", "text/plain", "404 Not Found");
     }
@@ -44,9 +65,11 @@ function handleClient(socket) {
 function parseRequest(request) {
   const lines = request.split("\r\n");
   const path = lines[0].split(" ")[1];
+  const requestType = lines[0].split(" ")[0];
   const userAgentLine = lines.find((line) => line.startsWith("User-Agent: "));
   const userAgent = userAgentLine ? userAgentLine.substring(12) : "Unknown";
-  return { path, userAgent };
+  const body = lines[lines.length - 1];
+  return { requestType, path, userAgent, body };
 }
 
 function sendResponse(socket, statusCode, statusText, contentType, content) {
