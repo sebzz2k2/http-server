6aabbb2046cccae8ef9acb36c031c89dec72a0c9
Sebin CM <sebinsebzz2002@gmail.com>
2023-10-27 15:24:26 +0530

    Parse headers
    refactor code

--- app/main.js
+++ app/main.js
@@ -1,27 +1,44 @@
 const net = require("net");
 
-const server = net.createServer((socket) => {
-    socket.on("data", (stream) => {
-        const data = stream.toString();
-        const path = data.split(" ")[1];
-        if (path === "/") {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        }else if(path.substring(1,5) === "echo"){
-            socket.write("HTTP/1.1 200 OK\r\n");
-            socket.write("Content-Type: text/plain\r\n");
-            socket.write("Content-Length: " + path.substring(6).length + "\r\n\r\n")
-            socket.write(path.substring(6)+"\r\n");
-            socket.end();
-        }else{
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        }
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        socket.end()
-    });
-    socket.on("close", () => {
-        socket.end();
-        server.close();
-    });
-});
+const server = net.createServer(handleClient);
+
+function handleClient(socket) {
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const { path, userAgent } = parseRequest(request);
+
+    if (path === "/") {
+      sendResponse(socket, 200, "OK", "text/plain", "");
+    } else if (path.startsWith("/echo/")) {
+      const content = path.substring(6);
+      sendResponse(socket, 200, "OK", "text/plain", content);
+    } else if (path === "/user-agent") {
+      sendResponse(socket, 200, "OK", "text/plain", userAgent);
+    } else {
+      sendResponse(socket, 404, "Not Found", "text/plain", "404 Not Found");
+    }
+  });
+
+  socket.on("close", () => {
+    socket.end();
+  });
+}
+
+function parseRequest(request) {
+  const lines = request.split("\r\n");
+  const path = lines[0].split(" ")[1];
+  const userAgentLine = lines.find((line) => line.startsWith("User-Agent: "));
+  const userAgent = userAgentLine ? userAgentLine.substring(12) : "Unknown";
+  return { path, userAgent };
+}
+
+function sendResponse(socket, statusCode, statusText, contentType, content) {
+  const response =
+    `HTTP/1.1 ${statusCode} ${statusText}\r\n` +
+    `Content-Type: ${contentType}\r\n` +
+    `Content-Length: ${content.length}\r\n\r\n${content}\r\n`;
+
+  socket.end(response);
+}
 
 server.listen(4221, "localhost");
