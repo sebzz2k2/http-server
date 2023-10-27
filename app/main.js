6a655f82af5cdaa95e7018fc9afa094c2da7e4e1
Sebin CM <sebinsebzz2002@gmail.com>
2023-10-27 14:23:40 +0530

    Respond with 404

--- app/main.js
+++ app/main.js
@@ -2,6 +2,13 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
     socket.on("data", (stream) => {
+        const data = stream.toString();
+        const path = data.split(" ")[1];
+        if (path === "/") {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        }else{
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
         socket.end()
     });
