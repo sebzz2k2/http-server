a2bc5737b40f79d50c8b039864af5ee9f8ebaae8
Sebin CM <sebinsebzz2002@gmail.com>
2023-10-27 16:06:52 +0530

    use promises

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
 const net = require("net");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const server = net.createServer(handleClient);
 
 function handleClient(socket) {
-  socket.on("data", (data) => {
+  socket.on("data", async (data) => {
     const request = data.toString();
     const { requestType, path, userAgent, body } = parseRequest(request);
 
@@ -16,41 +16,22 @@ function handleClient(socket) {
     } else if (path === "/user-agent") {
       sendResponse(socket, 200, "OK", "text/plain", userAgent);
     } else if (path.startsWith("/files/")) {
-      const dirIndex = process.argv.indexOf("--directory") + 1;
-      if (dirIndex === 0) {
+      const dir = process.argv[process.argv.indexOf("--directory") + 1];
+      if (!dir) {
         socket.end();
+        return;
       }
-      const dir = process.argv[dirIndex];
-      const fileName = path.substring(7);
-      if (requestType === "GET") {
-        fs.readFile(`${dir}/${fileName}`, (err, data) => {
-          if (err) {
-            sendResponse(
-              socket,
-              404,
-              "Not Found",
-              "text/plain",
-              "404 Not Found"
-            );
-            return;
-          }
+
+      try {
+        if (requestType === "GET") {
+          const data = await getFileContent(dir, path.substring(7));
           sendResponse(socket, 200, "OK", "application/octet-stream", data);
-        });
-      }
-      if (requestType === "POST") {
-        fs.writeFile(`${dir}/${fileName}`, body, (err) => {
-          if (err) {
-            sendResponse(
-              socket,
-              404,
-              "Not Found",
-              "text/plain",
-              "404 Not Found"
-            );
-            return;
-          }
-          sendResponse(socket, 201, "OK", "text/plain", body);
-        });
+        } else if (requestType === "POST") {
+          await writeFileContent(dir, path.substring(7), body);
+          sendResponse(socket, 201, "Created", "text/plain", body);
+        }
+      } catch (err) {
+        sendResponse(socket, 404, "Not Found", "text/plain", "404 Not Found");
       }
     } else {
       sendResponse(socket, 404, "Not Found", "text/plain", "404 Not Found");
@@ -64,12 +45,22 @@ function handleClient(socket) {
 
 function parseRequest(request) {
   const lines = request.split("\r\n");
-  const path = lines[0].split(" ")[1];
   const requestType = lines[0].split(" ")[0];
+  const path = lines[0].split(" ")[1];
   const userAgentLine = lines.find((line) => line.startsWith("User-Agent: "));
   const userAgent = userAgentLine ? userAgentLine.substring(12) : "Unknown";
   const body = lines[lines.length - 1];
   return { requestType, path, userAgent, body };
+}
+
+async function getFileContent(dir, fileName) {
+  const filePath = `${dir}/${fileName}`;
+  return await fs.readFile(filePath);
+}
+
+async function writeFileContent(dir, fileName, data) {
+  const filePath = `${dir}/${fileName}`;
+  await fs.writeFile(filePath, data);
 }
 
 function sendResponse(socket, statusCode, statusText, contentType, content) {
